ac889f386c46a287f59dfc84557403a35b0b57ae
Upadhyay-Yatendra <yupadhyayyk@gmail.com>
2022-09-17 04:46:43 +0530

    2ND-COMMIT

---
+++ index.js
@@ -0,0 +1,64 @@
+function makeSound(key)
+{
+    switch (key) 
+    {
+      case "a":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+
+      case "s":
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+
+      case "d":
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+      case "f":
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+      case "j":
+        var tom5 = new Audio("sounds/snare.mp3");
+        tom5.play();
+        break;
+      case "k":
+        var tom6 = new Audio("sounds/crash.mp3");
+        tom6.play();
+        break;
+      case "l":
+        var tom7 = new Audio("sounds/kick-bass.mp3");
+        tom7.play();
+        break;
+      default:
+        alert(key);
+        break;
+  }  
+}
+
+var i = 0;
+var numberOfDrums = document.querySelectorAll(".drum").length; 
+// since all buttons have a class= drum
+//   alert(numberOfDrums)
+
+for ( i = 0;  i < numberOfDrums; i++ ) 
+// i<7 will also work since we know no of buttons
+{
+  // ----------------- detecting button press-----------------------
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () 
+  {
+    makeSound(this.innerHTML);
+  });
+
+  
+  // we could have used an anonymous function here.............
+}
+
+// -----------------detecting key press--------------------------
+document.addEventListener("keydown",function(event)
+{
+  makeSound(event.key);
+});
+
